4cef964abe2a158caad8ca7837c2bbeba48c6d52
jun-ia <jundullah.ilhaq@gmail.com>
2022-11-25 15:21:35 +0700

    Initial Commit

---
+++ Netplix/NetplixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetplixApp.swift
+//  Netplix
+//
+//  Created by Jundullah Ilhaq Aulia on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NetplixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
